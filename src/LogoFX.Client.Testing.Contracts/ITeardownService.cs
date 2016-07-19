0cac3eff5b5865e6397bf11c83d04bc28b72fb75
godrose <godrose@gmail.com>
2016-07-19 10:25:47 +0000

    Added setup and teardown services.

---
+++ src/LogoFX.Client.Testing.Contracts/ITeardownService.cs
@@ -0,0 +1,14 @@
+﻿namespace LogoFX.Client.Testing.Contracts
+{
+    //TODO: Move to Attest.
+    /// <summary>
+    /// This service encapsulates functionality which is called when the test/scenario completes.
+    /// </summary>
+    public interface ITeardownService
+    {
+        /// <summary>
+        /// Teardowns this instance.
+        /// </summary>
+        void Teardown();
+    }
+}
